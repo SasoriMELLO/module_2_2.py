965772ca4a7c519acd16f3ae676f1dcd2f520603
SasoriMELLO <sasorimello@gmail.com>
2024-10-01 05:38:27 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first=6
+second=3
+third=3
+if first==second and second==third and third==first:
+    print(3)
+elif first==second or second==third or third==first:
+    print(2)
+else:
+    print(0)
